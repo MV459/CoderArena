cac8355d09e2297cfdf1a3fcf3edfc8e3be1276a
MV459 <saiavinashmandava45@gmail.com>
2024-08-11 23:26:34 +0530

    executeCpp.js

--- backend/utils/executeJava.js
+++ backend/utils/executeJava.js
@@ -1,3 +1,33 @@
+// // const fs = require('fs');
+// // const path = require('path');
+// // const { exec } = require('child_process');
+
+// // const outputPath = path.join(__dirname, 'outputs');
+// // if (!fs.existsSync(outputPath)) {
+// //     fs.mkdirSync(outputPath, { recursive: true });
+// // }
+
+// // const executeJava = async (filePath, inputPath) => {
+// //     const jobId = path.basename(filePath).split(".")[0];
+// //     const className = path.basename(filePath).split(".")[0];
+// //     const command = `javac ${filePath} && java -cp ${path.dirname(filePath)} ${className}`;
+// //     const executeCommand = inputPath ? `${command} < ${inputPath}` : command;
+
+// //     return new Promise((resolve, reject) => {
+// //         exec(executeCommand, (error, stdout, stderr) => {
+// //             if (error) {
+// //                 reject({ error, stderr });
+// //             } else if (stderr) {
+// //                 reject(stderr);
+// //             } else {
+// //                 resolve(stdout);
+// //             }
+// //         });
+// //     });
+// // };
+
+// // module.exports = executeJava;
+
 // const fs = require('fs');
 // const path = require('path');
 // const { exec } = require('child_process');
@@ -7,21 +37,26 @@
 //     fs.mkdirSync(outputPath, { recursive: true });
 // }
 
-// const executeJava = async (filePath, inputPath) => {
+// const executeJava = async (filePath) => {
 //     const jobId = path.basename(filePath).split(".")[0];
 //     const className = path.basename(filePath).split(".")[0];
-//     const command = `javac ${filePath} && java -cp ${path.dirname(filePath)} ${className}`;
-//     const executeCommand = inputPath ? `${command} < ${inputPath}` : command;
-
+    
 //     return new Promise((resolve, reject) => {
-//         exec(executeCommand, (error, stdout, stderr) => {
-//             if (error) {
-//                 reject({ error, stderr });
-//             } else if (stderr) {
-//                 reject(stderr);
-//             } else {
-//                 resolve(stdout);
+//         // Compile the Java file
+//         exec(`javac ${filePath}`, (compileError, stdout, stderr) => {
+//             if (compileError) {
+//                 reject({ error: compileError.message, stderr });
+//                 return;
 //             }
+            
+//             // Run the compiled class file
+//             exec(`java -cp ${path.dirname(filePath)} ${className}`, (runError, runStdout, runStderr) => {
+//                 if (runError) {
+//                     reject({ error: runError.message, stderr: runStderr });
+//                     return;
+//                 }
+//                 resolve(runStdout);
+//             });
 //         });
 //     });
 // };
@@ -62,3 +97,4 @@ const executeJava = async (filePath) => {
 };
 
 module.exports = executeJava;
+
